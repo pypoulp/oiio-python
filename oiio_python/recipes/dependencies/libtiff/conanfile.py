9a5bac61543d6f08f79a8168faba135d4a915f60
Paul Parneix <29799363+pypoulp@users.noreply.github.com>
2025-06-10 21:34:57 +0200

    Cmake issue on Linux builds

--- oiio_python/recipes/dependencies/libtiff/conanfile.py
+++ oiio_python/recipes/dependencies/libtiff/conanfile.py
@@ -1,7 +1,6 @@
 import os
 
 from conan import ConanFile
-from conan.errors import ConanInvalidConfiguration
 from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
 from conan.tools.files import (
     apply_conandata_patches,
@@ -82,7 +81,7 @@ class LibtiffConan(ConanFile):
         self.requires("libwebp/1.4.0")
 
     def build_requirements(self):
-        self.tool_requires("cmake/[>=3.19 <4]")
+        self.tool_requires("cmake/[>=3.18 <4]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
